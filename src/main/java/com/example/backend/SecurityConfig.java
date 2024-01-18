cccac55c8099abec063a225783a910c9bc0f65fc
howard <howardwang8998@gmail.com>
2024-01-19 01:07:42 +0800

    doing this is registering a SecurityFilterChain bean

---
+++ src/main/java/com/example/backend/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.backend.common.AuthorizationCheckFilter;
+
+@Configuration
+public class SecurityConfig{
+    @Bean
+    public  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 在這裡進行相關的安全配置
+        http
+            .authorizeHttpRequests(requests ->requests
+                .requestMatchers("/public/**").permitAll()
+                .anyRequest().authenticated())
+            .formLogin(login ->login
+                .loginPage("/login")
+                .permitAll())
+            .logout(logout ->logout
+                .permitAll())
+            .csrf(csrf ->csrf.disable())
+            .sessionManagement(management ->management
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(new AuthorizationCheckFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
