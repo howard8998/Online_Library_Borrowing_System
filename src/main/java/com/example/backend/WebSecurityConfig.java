a79e79ea2b351054c3001e2894eb9e65464f39d6
howard <howardwang8998@gmail.com>
2024-01-19 00:39:31 +0800

    加入jwt token大概

---
+++ src/main/java/com/example/backend/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.example.backend.common.AuthorizationCheckFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfiguration {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        // 關閉 CSRF 保護
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new AuthorizationCheckFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
